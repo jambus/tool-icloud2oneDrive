2921be653fb679aa3ac9bc8f6928d86e7fea0e3d
jambus <jambus2010@gmail.com>
2024-01-14 21:36:59 +0800

    New feature to check if file contains originate shot time, if not, it will use the specific date parameter

--- updateAllWithShotTime.sh
+++ updateAllWithShotTime.sh
@@ -19,23 +19,48 @@ trap finish EXIT
 echo "Start process"
 
 # Specify the time you want to set
-# TODO time_to_set="2022:02:16 12:50:00"
+time_to_set="2022:02:16 12:50:00"
 
 # 检查当前目录下是否有文件
 if ls -1qA * 2>/dev/null | grep -q .
 
 then
-    # Loop over all JPG, MOV & MP4 files in the current directory
-    jhead -ft *.jpeg
-    jhead -ft *.JPG
+    # Loop over all files in the current directory
+    for file in *
+    do
+        # Get the file extension
+        extension="${file##*.}"
+        
+        # Convert the extension to lowercase
+        extension=$(echo "$extension" | tr '[:upper:]' '[:lower:]')
+        
+        # Check the file type and perform the related update
+        case "$extension" in
+            jpg|jpeg|png)
+                # Check if the DateTimeOriginal tag is set
+                if ! exiftool -DateTimeOriginal "$file" | grep -q "DateTimeOriginal"
+                then
+                    # If the DateTimeOriginal tag is not set, update it with the specified time
+                    exiftool -overwrite_original -DateTimeOriginal="$time_to_set" "$file"
+                fi
+                exiftool -overwrite_original "-FileModifyDate<DateTimeOriginal" -d "%Y:%m:%d %H:%M:%S" "$file"
+                ;;
+            mov|mp4)
+                exiftool -overwrite_original "-FileModifyDate<MediaCreateDate" -d "%Y:%m:%d %H:%M:%S" "$file"
+                ;;
+        esac
+    done
 
-    #exiftool -MediaCreateDate="2022:02:16 12:50:00" *.MOV 
-    #exiftool -DateTimeOriginal="2021:02:16 12:50:00" *.PNG
-    exiftool "-FileModifyDate<DateTimeOriginal" -d "%Y:%m:%d %H:%M:%S" *.PNG
-    exiftool "-FileModifyDate<MediaCreateDate" -d "%Y:%m:%d %H:%M:%S" *.MOV
-    exiftool "-FileModifyDate<MediaCreateDate" -d "%Y:%m:%d %H:%M:%S" *.mov
+    #exiftool -MediaCreateDate="2022:04:05 11:00:00" *.MOV 
+    #exiftool -DateTimeOriginal="2022:04:05 11:00:00" *.PNG
+    #exiftool -DateTimeOriginal="2022:04:05 11:00:00" *.jpeg
+    #exiftool "-FileModifyDate<DateTimeOriginal" -d "%Y:%m:%d %H:%M:%S" *.jpeg
+    #exiftool "-FileModifyDate<DateTimeOriginal" -d "%Y:%m:%d %H:%M:%S" *.JPG
+    #exiftool "-FileModifyDate<DateTimeOriginal" -d "%Y:%m:%d %H:%M:%S" *.PNG
+    #exiftool "-FileModifyDate<MediaCreateDate" -d "%Y:%m:%d %H:%M:%S" *.MOV
+    #exiftool "-FileModifyDate<MediaCreateDate" -d "%Y:%m:%d %H:%M:%S" *.mov
 
-    exiftool "-FileModifyDate<MediaCreateDate" -d "%Y:%m:%d %H:%M:%S" *.mp4
+    #exiftool "-FileModifyDate<MediaCreateDate" -d "%Y:%m:%d %H:%M:%S" *.mp4
     echo "Timestamps updated for all files in the current directory."
 else
     echo "No files found in the current directory."
